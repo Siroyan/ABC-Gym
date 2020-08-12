be7c6c7969e1e60b592bfc840691e285b8c13f3a
Siroyan <1517053@ed.tus.ac.jp>
2020-08-12 06:16:17 +0000

    [fix] ファイル名とプログラムの不一致

--- 172/B.cpp
+++ 172/B.cpp
@@ -4,40 +4,14 @@ using namespace std;
 
 int main() {    
     /* Input */
-    long long int N, M, K;
-    cin >> N >> M >> K;
-    
-    vector<long long int> A(N);
-    vector<long long int> B(M);
-    rep (i, N) {
-        cin >> A.at(i);
-    }
-    rep (i, M) {
-        cin >> B.at(i);
-    }
-
+    string S, T;
+    cin >> S;
+    cin >> T;
     /* Main Logic */
-    vector<long long int> sumA(N+1);
-    vector<long long int> sumB(M+1);
-    rep (i, N+1) {
-        if (i == 0) sumA.at(i) = 0;
-        else sumA.at(i) = sumA.at(i-1) + A.at(i-1);
-    }
-    rep (i, M+1) {
-        if (i == 0) sumB.at(i) = 0;
-        else sumB.at(i) = sumB.at(i-1) + B.at(i-1);
-    }
-
     int ans = 0;
-    int j = M;
-    for (int i = 0; i < N + 1; i++) {
-        if (sumA.at(i) > K) break;
-        while ( sumB.at(j) > K - sumA.at(i) ) {
-            j--;
-        }
-        ans = max(ans, i + j);
+    rep (i, S.length()) {
+        if (S[i] != T[i]) ans++;
     }
-
     /* Output */
     cout << ans << endl;
     return 0;
